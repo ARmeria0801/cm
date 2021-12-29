651476d7f6f626e6dad86c032c97d98a4576f16d
kimis0719 <74697777+kimis0719@users.noreply.github.com>
2021-12-29 16:32:28 +0900

    20211229
    20211229

--- src/main/java/com/myshop/cm/controller/MypageController.java
+++ src/main/java/com/myshop/cm/controller/MypageController.java
@@ -1,8 +1,5 @@
 package com.myshop.cm.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,22 +7,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.OrderComparator.OrderSourceProvider;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.myshop.cm.model.DeliveryAddressVO;
-import com.myshop.cm.model.DeliveryTemplateVO;
-import com.myshop.cm.model.GoodsVO;
-import com.myshop.cm.model.MemberVO;
-import com.myshop.cm.model.OptionVO;
-import com.myshop.cm.model.OrderVO;
-import com.myshop.cm.model.ReviewVO;
+import com.myshop.cm.model.OrderListVO;
 import com.myshop.cm.service.DeliveryAddressService;
 import com.myshop.cm.service.DeliveryTemplateService;
 import com.myshop.cm.service.GoodsService;
@@ -70,7 +56,7 @@ public class MypageController {
 			String page = request.getParameter("page");
 			
 			// 주문 상세정보 구해오기
-			OrderVO order = orderService.getOrderDetail(ord_num);
+			OrderListVO order = orderService.getOrderDetail(ord_num);
 			
 			// 주문 상세정보의 gds_num으로 상품정보 불러오기
 //			GoodsVO goods = goodsService.goodsdetail(order.getOrd_gdsnum());
